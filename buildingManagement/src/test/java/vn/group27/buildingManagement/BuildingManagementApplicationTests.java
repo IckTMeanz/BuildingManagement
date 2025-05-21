007478eada91e6285a45ef64cda7db7780d52d63
nghiaduong <duongnghia2904204@gmail.com>
2025-05-21 20:08:43 +0700

    src format

---
+++ buildingManagement/src/test/java/vn/group27/buildingManagement/BuildingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.group27.buildingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
